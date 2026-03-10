596f5c8b2071a8fc118cb86cf31d5edd2d8299a0
morning2026 <13505977168@163.com>
2026-03-11 03:52:49 +0800

    短信登录-登录校验拦截器

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.hmdp.utils;
+
+import com.hmdp.entity.User;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.获取session
+        HttpSession session = request.getSession();
+        // 2.获取session中的用户
+        Object user = session.getAttribute("user");
+        // 3.判断用户是否存在
+        if (user == null) {
+            // 4.用户不存在，返回401状态码，未授权
+            response.setStatus(401);
+            return false;
+        }
+
+        // 5.用户存在，保存用户到ThreadLocal
+        UserHolder.saveUser((User) user);
+        // 6.放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
